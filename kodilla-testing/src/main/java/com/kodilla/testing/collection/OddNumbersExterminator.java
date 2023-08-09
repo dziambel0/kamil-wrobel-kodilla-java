f58b39b5d8ab07d400bce147fcb347012de2aaca
Kamil Wrobel <wrobelkam@gmail.com>
2023-08-09 21:33:08 +0200

    6.3 - test klasy wybierającej parzyste liczby z kolekcji

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,6 +3,7 @@ package com.kodilla.testing.collection;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class OddNumbersExterminator {
 
     List<Integer> lista = new ArrayList<>();
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/collection/CollectionTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class CollectionTestSuite {
 
     @BeforeEach
