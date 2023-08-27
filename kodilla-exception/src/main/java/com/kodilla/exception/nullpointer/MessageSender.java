8fa4794119c6e45ddff9cb9784f90cb81dbaede5
Kamil Wrobel <wrobelkam@gmail.com>
2023-08-27 10:03:32 +0200

    dodatkowe wyjatki

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/nullpointer/MessageSender.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.nullpointer;
+
+public class MessageSender {
+
+    public void sendMessageTo(User user, String massage) throws MessageNotSentException{
+        if (user != null){
+            System.out.println("Sending message: " + massage + " to: " + user.getName());
+        }else {
+            throw new MessageNotSentException("Obiect was null");
+        }
+
+    }
+
+}
