c32092f755f21f138d34f5b12725120b0ac4b8cd
Kamil Wrobel <wrobelkam@gmail.com>
2023-11-06 21:10:21 +0100

    17.3 ZALEZNOSCI 2

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -0,0 +1,50 @@
+package com.kodilla.hibernate.manytomany;
+
+import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "COMPANIES")
+public class Company {
+    private int id;
+    private String name;
+    private List<Employee> employeeList = new ArrayList<>();
+
+    public Company(){
+    }
+
+    public Company(String name) {
+        this.name = name;
+    }
+@Id
+@GeneratedValue
+@NonNull
+@Column(name = "COMPANY_ID", unique = true)
+    public int getId() {
+        return id;
+    }
+@NonNull
+@Column(name = "COMPANY_NAME")
+    public String getName() {
+        return name;
+    }
+@ManyToMany(cascade = CascadeType.ALL, mappedBy = "companyList")
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -0,0 +1,62 @@
+package com.kodilla.hibernate.manytomany.dao;
+
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CompanyDaoTestSuite {
+
+    @Autowired
+    private CompanyDao companyDao;
+
+    @Test
+    void testSaveManyToMany() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployeeList().add(johnSmith);
+        dataMaesters.getEmployeeList().add(stephanieClarckson);
+        dataMaesters.getEmployeeList().add(lindaKovalsky);
+        greyMatter.getEmployeeList().add(johnSmith);
+        greyMatter.getEmployeeList().add(lindaKovalsky);
+
+        johnSmith.getCompanyList().add(softwareMachine);
+        johnSmith.getCompanyList().add(greyMatter);
+        stephanieClarckson.getCompanyList().add(dataMaesters);
+        lindaKovalsky.getCompanyList().add(dataMaesters);
+        lindaKovalsky.getCompanyList().add(greyMatter);
+
+        //When
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaesters);
+        int dataMaestersId = dataMaesters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        //Then
+        assertNotEquals(0, softwareMachineId);
+        assertNotEquals(0, dataMaestersId);
+        assertNotEquals(0, greyMatterId);
+
+        //CleanUp
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaestersId);
+            companyDao.deleteById(greyMatterId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+}
