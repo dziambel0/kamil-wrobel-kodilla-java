e49c22ef566aacc7d663f8b70ccbdcadfcf21386
Kamil Wrobel <wrobelkam@gmail.com>
2023-11-09 19:16:57 +0100

    17.4 HQL

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -64,49 +64,7 @@ class CompanyDaoTestSuite {
             //do nothing
         }
     }
-    @Test
-    void testEmployeeLastName(){
-        Employee johnSmith = new Employee("John", "Smith");
-        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
-        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
 
-        Company softwareMachine = new Company("Software Machine");
-        Company dataMaesters = new Company("Data Maesters");
-        Company greyMatter = new Company("Grey Matter");
-
-        softwareMachine.getEmployeeList().add(johnSmith);
-        dataMaesters.getEmployeeList().add(stephanieClarckson);
-        dataMaesters.getEmployeeList().add(lindaKovalsky);
-        greyMatter.getEmployeeList().add(johnSmith);
-        greyMatter.getEmployeeList().add(lindaKovalsky);
-
-        johnSmith.getCompanyList().add(softwareMachine);
-        johnSmith.getCompanyList().add(greyMatter);
-        stephanieClarckson.getCompanyList().add(dataMaesters);
-        lindaKovalsky.getCompanyList().add(dataMaesters);
-        lindaKovalsky.getCompanyList().add(greyMatter);
-
-        //When
-        companyDao.save(softwareMachine);
-        int softwareMachineId = softwareMachine.getId();
-        companyDao.save(dataMaesters);
-        int dataMaestersId = dataMaesters.getId();
-        companyDao.save(greyMatter);
-        int greyMatterId = greyMatter.getId();
-
-        //Then
-
-
-        //CleanUp
-        try {
-            companyDao.deleteById(softwareMachineId);
-            companyDao.deleteById(dataMaestersId);
-            companyDao.deleteById(greyMatterId);
-        } catch (Exception e) {
-            //do nothing
-        }
-
-    }
     @Test
     void testEmployeeLastNameAndCompanyThreeLeter(){
         Employee johnSmith = new Employee("John", "Smith");
